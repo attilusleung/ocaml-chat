04b0ba456067902bd5148668bb9d26a3cfc99c45
Attilus Leung <attilusleung@gmail.com>
2019-11-04 16:07:29 -0500

    simple server with debug mode

---
+++ src/server/server.ml
@@ -0,0 +1,113 @@
+open Lwt
+open Lwt_io
+open Printexc
+
+exception ClosedConnection
+
+module Server = struct
+  type connection =
+    { file: Lwt_unix.file_descr
+    ; in_channel: Lwt_io.input_channel
+    ; out_channel: Lwt_io.output_channel
+    ; sockadd: Lwt_unix.sockaddr
+    }
+
+end
+
+type connection =
+  { file: Lwt_unix.file_descr
+  ; in_channel: Lwt_io.input_channel
+  ; out_channel: Lwt_io.output_channel
+  ; sockadd: Lwt_unix.sockaddr }
+
+(* TODO: Get an actual ip address (currently points to localhost) *)
+let listen_address = Unix.inet_addr_loopback
+
+let port = 9000
+
+let backlog = 5
+
+(* https://baturin.org/code/lwt-counter-server/ *)
+let rec handle_connection ic oc id () =
+  (* Lwt_io.read_line_opt ic >>= *)
+  (* (function *)
+  (*   | Some msg -> print_endline "something" >>= connection ic oc *)
+  (*   | None -> print_endline "end" >>= return *)
+  (* ) *)
+  let%lwt line = read_line_opt ic in
+  match line with
+  | Some msg ->
+    Lwt_io.write_line stdout @@ "recieved: \"" ^ msg ^ "\" from " ^ id
+    >>= fun () ->
+    Lwt_io.write_line oc "recieved" >>= handle_connection ic oc id
+  | None ->
+    Lwt_io.write_line stdout @@ "connection " ^ id ^ " terminated"
+    >>= fun () -> fail ClosedConnection
+
+(* let accept_connection conn =  *)
+
+let accept_connection active conn =
+  let fd, sa = conn in
+  let ic = Lwt_io.of_fd Lwt_io.Input fd in
+  let oc = Lwt_io.of_fd Lwt_io.Output fd in
+  let id =
+    match sa with
+    | Unix.ADDR_INET (a, p) ->
+      Unix.string_of_inet_addr a ^ ":" ^ string_of_int p
+    | Unix.ADDR_UNIX _ ->
+      failwith "unreachable code"
+  in
+  let connection_rec =
+    {file= fd; in_channel= ic; out_channel= oc; sockadd= sa}
+  in
+  Hashtbl.add active id connection_rec ;
+  Lwt.on_failure (handle_connection ic oc id ()) (fun e ->
+      ( match e with
+        | ClosedConnection ->
+          print_endline @@ "Connection with " ^ id ^ " closed"
+        | e ->
+          print_endline @@ "An error occured :" ^ to_string e ) ;
+      Hashtbl.remove active id) ;
+  let%lwt () = Lwt_io.write_line stdout @@ "new connection from " ^ id in
+  return ()
+
+let rec debug_input active () =
+  let%lwt input = Lwt_io.read_line_opt Lwt_io.stdin in
+  ( match input with
+    | Some s -> (
+        match String.split_on_char ' ' s with
+        | h :: t -> (
+            match Hashtbl.find_opt active h with
+            | Some {out_channel= oc} ->
+              Lwt_io.write_line oc @@ String.concat " " t
+            | None ->
+              Lwt_io.write_line Lwt_io.stdout @@ "No active connection " ^ h )
+        | _ ->
+          Lwt_io.write_line Lwt_io.stdout "Malformed function" )
+    | None ->
+      return () )
+  >>= debug_input active
+
+let create_server sock =
+  let active = Hashtbl.create 5 in
+  ignore @@ debug_input active () ;
+  let rec serve active () =
+    Lwt_unix.accept sock >>= accept_connection active >>= serve active
+  in
+  serve active
+
+let create_socket () =
+  Lwt_unix.(
+    let sock = socket PF_INET SOCK_STREAM 0 in
+    (* let%lwt () = bind sock @@ ADDR_INET (listen_address, port) in *)
+    ignore @@ bind sock @@ ADDR_INET (listen_address, port) ;
+    listen sock backlog ;
+    sock)
+
+let () =
+  let sock = create_socket () in
+  let serve = create_server sock in
+  print_endline @@ "Server started at "
+                   ^ Unix.string_of_inet_addr listen_address
+                   ^ ":" ^ string_of_int port ;
+  Lwt_main.run @@ serve ()
