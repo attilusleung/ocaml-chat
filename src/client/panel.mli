c7090d661543961571295f321be628cc6e922543
Attilus Leung <attilusleung@gmail.com>
2019-11-04 16:05:54 -0500

    added msg pannel and made some functions threaded

--- src/client/panel.mli
+++ src/client/panel.mli
@@ -1,3 +1,5 @@
+exception PanelWidthTooLarge
+exception PanelHeightTooLarge
 
 module Panel: sig
   type t
@@ -9,6 +11,12 @@ module InputPanel : sig
   type t
   val make: int -> int -> int -> int -> t
   val draw: t -> string array array -> bool -> unit
-  val update: t -> Key.key -> unit
+  val update: t -> Network.connection -> Key.key -> unit Lwt.t
   val get_cursor: t-> int * int
+end
+
+module MessagePanel : sig
+  type t
+  val make: int -> int -> int -> int -> t * string DoublyLinkedList.t ref
+  val draw: t -> string array array -> bool -> unit
 end
