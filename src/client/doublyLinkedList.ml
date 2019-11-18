295bfcda269d4b7977d279d2893e2c9057cc45e8
Attilus Leung <attilusleung@gmail.com>
2019-11-18 09:15:24 -0500

    swapped next and prev for doubly linked list, added documentation

--- src/client/doublyLinkedList.ml
+++ src/client/doublyLinkedList.ml
@@ -2,99 +2,114 @@
 open Log
 
 exception EndOfList
+
 exception EmptyList
 
-type 'a entry =
-  { value: 'a; mutable backptr: 'a entry list option }
+exception NotHead
+
+type 'a entry = {value: 'a; mutable backptr: 'a entry list option}
+(** Represents an entry in the doubly linked list. It contains a value of the
+ * element, as well as a backpointer to the next element in the list *)
 
 type 'a t = 'a entry list
-(* AF: the list [{value=v1; backptr=b1}; ...; {value=vn; backptr=bn}] represents
-                  * the list [v1; ... vn]
-                  *
-                  * RI: For all entry ei in the list [e1; ...; en], e_i.backptr points to the
-                  * Some list [e(i-1); ei; ...; en]. e1.backptr should point to None.
-                 *)
+(** The abstract type of the doubly linked list. It is just a list of entries.
+ * *)
+
+(* AF: the list
+ * [{value=v1; backptr=b1}; ...; {value=vn; backptr=bn}]
+      represents the list [v1; ... vn]
+
+      RI: For all entry ei in the list [e1; ...; en], e_i.backptr points to the
+      Some list [e(i-1); ei; ...; en]. e1.backptr should point to None.
+    *)
 
 let empty = []
 
+(** [rep_ok t] is [t] if [t] satisfies the representation invariant. It fails
+    with an assertion error otherwise *)
 let rep_ok t =
   match t with
   (* RI satisfied if empty list *)
-  | [] -> true
+  | [] ->
+    t
   (* Check first element backptr is None *)
-  | h::ta -> if h.backptr <> None then false else
+  | h :: ta ->
+    if h.backptr <> None then assert false
+    else
       (* [pointer_ok t] is whether t has valid pointers *)
       let rec pointer_ok t =
         match t with
-        | [] -> true
-        | _::ta as l ->
-          (* Nested match statements are necessary to preserve physical equality
-           * of variables *)
-          match ta with
-          | [] -> true
-          (* Checks if backpointer of the second element in the list points to
-           * the head of the list *)
-          |{backptr= Some b}::_ -> if b == l then
-              pointer_ok ta else false
-          (* Only the first element of the list should have a None value for
-           * backptr *)
-          |{backptr= None}::_ -> false
+        | [] ->
+          true
+        | _ :: ta as l -> (
+            (* Nested match statements are necessary to preserve physical equality
+             * of variables *)
+            match ta with
+            | [] ->
+              true
+            (* Checks if backpointer of the second element in the list points to
+             * the head of the list *)
+            | {backptr= Some b} :: _ ->
+              if b == l then pointer_ok ta else false
+            (* Only the first element of the list should have a None value for
+             * backptr *)
+            | {backptr= None} :: _ ->
+              false )
       in
-      pointer_ok t
+      assert (pointer_ok t) ;
+      t
 
 let is_empty t = t = empty
 
-let insert value t =
-  let l = {value= value; backptr= None}::t in
-  (match t with
-   | [] -> ()
-   | h::t -> (h.backptr <- Some l));
+let rec insert value t =
+  let l = {value; backptr= None} :: t in
+  ( match t with
+    | [] ->
+      ()
+    | ({backptr= None} as h) :: t ->
+      h.backptr <- Some l
+    | {backptr= Some p} :: _ ->
+      raise NotHead ) ;
   l
 
-let to_singly_list t =
-  List.map (fun a -> a.value) t
+let to_singly_list t = List.map (fun a -> a.value) t
 
-let get_value t =
-  match t with
-  | [] -> raise EmptyList
-  | h::t -> h.value
-
-let prev t =
-  match t with
-  | [] -> raise EmptyList
-  | {backptr= None}::_ -> raise EndOfList
-  | {backptr= Some p}::_ -> p
-
-let prev_opt t =
-  try
-    Some (prev t)
-  with
-  | EndOfList -> None
-  | EmptyList -> None
-
-let rec previ t i =
-  if i = 0 then t
-  else match t with
-    | {backptr= Some b}::_ -> previ b (i-1)
-    | {backptr= None}::_ -> raise EndOfList
-    | [] -> raise EmptyList
+let get_value t = match t with [] -> raise EmptyList | h :: t -> h.value
 
 let next t =
   match t with
-  | [] -> raise EmptyList
-  | h::[] -> raise EndOfList
-  | h::t -> t
+  | [] ->
+    raise EmptyList
+  | {backptr= None} :: _ ->
+    raise EndOfList
+  | {backptr= Some p} :: _ ->
+    p
 
-let next_opt t =
-  try
-    Some (next t)
-  with
-  | EndOfList -> None
-  | EmptyList -> None
+let next_opt t = try Some (next t) with EndOfList -> None | EmptyList -> None
 
 let rec nexti t i =
   if i = 0 then t
-  else match t with
-    | [] -> raise EmptyList
-    | h::[] -> raise EndOfList
-    | h::t -> nexti t (i-1)
+  else
+    match t with
+    | {backptr= Some b} :: _ ->
+      nexti b (i - 1)
+    | {backptr= None} :: _ ->
+      raise EndOfList
+    | [] ->
+      raise EmptyList
+
+let prev t =
+  match t with [] -> raise EmptyList | [h] -> raise EndOfList | h :: t -> t
+
+let prev_opt t = try Some (prev t) with EndOfList -> None | EmptyList -> None
+
+let rec previ t i =
+  if i = 0 then t
+  else
+    match t with
+    | [] ->
+      raise EmptyList
+    | [h] ->
+      raise EndOfList
+    | h :: t ->
+      previ t (i - 1)
--- src/client/doublyLinkedList.mli
+++ src/client/doublyLinkedList.mli
@@ -7,31 +7,63 @@
    number of elements without traversing the whole list.
 *)
 
-(** The abstract type of values representing the doubly linked list *)
+(* Yeah, I know this is a terrible way to implement a doubly linked list, and
+ * yeah, I know we probably could have used the standard implementation of
+ * having a record with two pointer. But this is fun. *)
+
 type 'a t
-(* type 'a entry = *)
-(*   { value: 'a; mutable backptr: 'a entry list option } *)
-(*  *)
-(* type 'a t = 'a entry list *)
+(** The abstract type of values representing a pointer to an element in a doubly
+    linked list.
+    The data structure is mutable -- inserting an element into the list changes
+    the list. The pointers, however, always points to the same element or node
+    regardless of how the list is changed.
+*)
 
-val empty: 'a t
+val empty : 'a t
+(** [empty] is an empty doubly linked list *)
 
-val is_empty: 'a t -> bool
+val is_empty : 'a t -> bool
+(** [is_empty l] is whether [l] is an empty doubly linked list *)
 
-val insert: 'a -> 'a t -> 'a t
+val insert : 'a -> 'a t -> 'a t
+(** [insert e l] is the pointer to the head of doubly linked list with [e]
+    appended to [l].
+    Requires: [l] needs to be a pointer to the head of the doubly linked list.
+    Otherwise, raises NotHead.
+*)
 
-val to_singly_list: 'a t -> 'a list
+val to_singly_list : 'a t -> 'a list
+(** [to_singly_list l] is the Ocaml List with the values of the doubly linked
+    list of [l] starting from the pointer [l] *)
 
-val get_value: 'a t -> 'a
+val get_value : 'a t -> 'a
+(** [get_value l] is the value of the element at the pointer [l] *)
 
-val prev: 'a t -> 'a t
+val prev : 'a t -> 'a t
+(** [prev l] is the pointer to the previous element of [l]
+    Raises: EndOfList if there is no previous element of [l].
+    Raises; EmptyList if [l] is empty *)
 
-val prev_opt: 'a t -> 'a t option
+val prev_opt : 'a t -> 'a t option
+(** [prev_opt l] is [Some p] if p is the pointer to the previous element of [l],
+    or [None] if the previous element of [l] does not exist or [l] is empty *)
 
-val previ: 'a t -> int -> 'a t
+val previ : 'a t -> int -> 'a t
+(** [previ i l] is the pointer to [i] elemenets previous of [l].
+    Raises: EndOfList if there is no ith previous element of [l].
+    Raises; EmptyList if [l] is empty *)
 
-val next: 'a t -> 'a t
+val next : 'a t -> 'a t
+(** [next l] is the pointer to the next element of [l]
+    Raises: EndOfList if there is no next element of [l].
+    Raises; EmptyList if [l] is empty *)
 
-val next_opt: 'a t -> 'a t option
 
-val nexti: 'a t -> int -> 'a t
+val next_opt : 'a t -> 'a t option
+(** [next_opt l] is [Some p] if p is the pointer to the next element of [l],
+    or [None] if the next element of [l] does not exist or [l] is empty *)
+
+val nexti : 'a t -> int -> 'a t
+(** [nexti i l] is the pointer to [i] elemenets next of [l].
+    Raises: EndOfList if there is no ith next element of [l].
+    Raises; EmptyList if [l] is empty *)
